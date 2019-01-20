6fb859e15446b092114126b969f7d64a424fc85d
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-01-20 04:52:12 -0500

    Create hex.php

---
+++ hex.php
@@ -0,0 +1,30 @@
+<?php
+// Coded By Afrizal F.A
+// Str To Hex & Hex To Str
+// Char Code Tool
+error_reporting(0);
+function strToHex($str){
+$hex='';
+for ($i=0; $i < strlen($str); $i++){
+$hex .= "\x".dechex(ord($str[$i]));
+}
+return $hex;
+}
+function hexToStr($hex){
+$str='';
+for ($i=0; $i < strlen($hex)-1; $i+=2){
+$str .= chr(hexdec($hex[$i].$hex[$i+1]));
+}
+return $str;
+}
+if($argv[1] == "hex"){
+$gzin = $argv[2];
+$char = strToHex($gzin);
+echo "\n$char\n";
+}
+if($argv[1] == "unhex"){
+$buff = str_replace('\x',"",$argv[2]);
+$char = hexToStr($buff);
+echo "\n$char\n";
+}
+?>
